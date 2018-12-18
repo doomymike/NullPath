d19441dd8cdbd0788d3eddba09ebc5827971b02d
doomymike <34379835+doomymike@users.noreply.github.com>
2018-12-18 10:46:12 -0500

    making basic constructors

--- src/Platform.java
+++ src/Platform.java
@@ -1,4 +1,21 @@
 
-public class Platform extends Item{
+import java.awt.Rectangle;
 
+public class Platform extends Item{
+	
+	int length,width;
+	Rectangle loc;
+	
+	Platform(int x, int y) {
+		super(x, y);
+	
+	}
+	
+	Platform(int x,int y, int width, int length){
+		super(x,y);
+		this.loc = new Rectangle(x,y,width,length);
+	}
+	
+	
 }
+
