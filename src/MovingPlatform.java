8bed7a3bab3d3f5dda3f557b4f662c35baff50a6
doomymike <34379835+doomymike@users.noreply.github.com>
2018-12-20 11:15:28 -0500

    added movement

--- src/MovingPlatform.java
+++ src/MovingPlatform.java
@@ -1,8 +1,10 @@
+//
+
 import java.awt.image.BufferedImage;
 
 public class MovingPlatform extends Platform{
 	
-	int x2,y2,direction,speed;
+	int x2,y2,x3,y3	,direction,speed; //x2y2 are bottom left x3y3 are top right
 	
 	MovingPlatform(int x, int y) {
 		super(x, y);
@@ -23,4 +25,37 @@ public class MovingPlatform extends Platform{
 	public static BufferedImage getSprite(){
 		return null;
 	}
+	
+	public void move(){
+		if(direction == 0){ //left
+			if(this.getX() <=x2){
+				direction = 2; //right
+				this.move();
+			}else{
+				this.setX(this.getX()-speed);
+			}
+		}else if(direction == 1){ //up
+			if(this.getY()+width >=y3){
+				direction = 3; //down
+				this.move();
+			}else{
+				this.setY(this.getY()+speed);
+			}
+		}else if(direction == 2){ //right
+			if(this.getY()+width >=x3){
+				direction = 0; //left
+				this.move();
+			}else{
+				this.setX(this.getX()+speed);
+			}
+		}else{ //down
+			if(this.getY()+width <=x2){
+				direction = 1; //down
+				this.move();
+			}else{
+				this.setY(this.getY()-speed);
+			}
+		}
+	}
 }
+
