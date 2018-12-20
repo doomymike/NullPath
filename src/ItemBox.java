a2017cfd48449f7ac58e8561cbf0e0a2b13ddcce
Li <072909997@EDU.YRDSB.NET>
2018-12-20 11:26:06 -0500

    merge

--- src/ItemBox.java
+++ src/ItemBox.java
@@ -1,6 +1,27 @@
 
 public class ItemBox {
 
+	private SimpleLinkedList<Item> items = new SimpleLinkedList<Item>();
 	
+	public ItemBox(SimpleLinkedList<Item> items) {
+		this.items = items;
+	}
+	
+	public ItemBox(int numItems) {
+		//Randomize items
+	}
+	
+	public Item removeItem(Item itemRemoved) {
+		items.remove(itemRemoved);
+		return itemRemoved;
+	}
+	
+	public void addItem(Item itemAdded) {
+		items.add(itemAdded);
+	}
+	
+	public void emptyBox() {
+		items.clear();
+	}
 	
 }
