eaff29429b308f9e788e0ae5e417c52d5ff4532a
Li <072909997@EDU.YRDSB.NET>
2018-12-20 11:18:42 -0500

    ?

--- src/Resources.java
+++ src/Resources.java
@@ -1,9 +1,18 @@
+//0 = left, 
+
 import java.io.BufferedImage;
 
 public class Resources {
 
 	private static SimpleLinkedList<Stage> stages = new SimpleLinkedList<Stage>();
 	//Add all stages to list
+	
+	private SimpleLinkedList<Character> characters = new SimpleLinkedList<Character>();
+	//Initialize character objects (will add players when players are assigned)
+	
+	private Stage currentStage;
+	
+	private SimpleLinkedList<Player> players = new SimpleLinkedList<Player>();
 	
 	Resources() {
 		//Default??
@@ -17,6 +26,41 @@ public class Resources {
 		if (stages.size() != 0) {
 			int rand = (int)(Math.round(Math.rand()*(stages.size()-1)));
 			return stages.get(rand);
+		}
+	}
+	
+	public SimpleLinkedList<Character> getCharacters() {
+		return characters;
+	}
+	
+	public Character getRandomCharacter() {
+		if (characters.size() != 0) {
+			int rand = (int)(Math.round(Math.rand()*(characters.size()-1)));
+			return characters.get(rand);
+		}
+	}
+	
+	public void setCurrentStage(Stage stage) {
+		currentStage = stage;
+	}
+	
+	public Stage getCurrentStage() {
+		return currentStage;
+	}
+	
+	public SimpleLinkedList<Player> getPlayers() {
+		return players;
+	}
+	
+	public void addPlayer(String name) {
+		players.add(new Player(name));
+	}
+	
+	public void removePlayer(String name) {
+		for (int i = 0; i < players.size(); i++) {
+			if (players.get(i).getName().equals(name)) {
+				players.remove(i);
+			}
 		}
 	}
 	
