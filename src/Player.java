dcb14296f792a27755a4122d256e63e42093fc6a
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-12-20 11:15:33 -0500

    Basic method addition

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,32 @@
 
 public class Player {
 
+	private String name;
+	private Character character;
+	private int score;
+	
+	public Player(String name) {
+		this.name = name;
+	}
+	
+	public void setCharacter(Character character) {
+		this.character = character;
+	}
+	
+	public Character getCharacter() {
+		return character;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
 }
