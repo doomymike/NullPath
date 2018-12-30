38ea7b97242b44cb164c92383830a0f0f3f5bf4a
bl21812 <39709270+bl21812@users.noreply.github.com>
2018-12-29 20:01:37 -0800

    bomb template

--- src/Bomb.java
+++ src/Bomb.java
@@ -1,3 +1,22 @@
-public class Bomb extends Item {
+import java.awt.BufferedImage;
 
+public class Bomb extends Item {
+  
+  private static BufferedImage sprite; //Need to set sprite
+  
+  public Bomb(int x, int y) {
+    super(x,y);
+  }
+  
+  public static int getRadius() {
+    return 25; //Placeholder radius
+  }
+  
+  public void explode() {
+  }
+  
+  public static BufferedImage getSprite() {
+    return sprite;
+  }
+  
 }
