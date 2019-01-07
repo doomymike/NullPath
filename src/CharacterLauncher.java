7689fef45dfc50a6cdfa537b8f3dbfc549e356e4
JL321 <james.liangyy@gmail.com>
2019-01-07 10:58:13 -0500

    Add files via upload

---
+++ src/CharacterLauncher.java
@@ -0,0 +1,20 @@
+import java.awt.image.BufferedImage;
+
+public class CharacterLauncher extends VelocityModifier{
+
+	static int speed = 30;
+	public CharacterLauncher(int x, int y, int height, int width, int direction, int direction2) {
+		// TODO Auto-generated constructor stub
+		super(x, y, height, width, direction, direction2);	
+	}
+
+	public void launchChar(Character affectChar) {
+		double[] updatedVel = {affectChar.getVelocity()[0], affectChar.getVelocity()[1]+speed};
+		affectChar.setVelocity(updatedVel);
+	}
+	
+	public static BufferedImage getSprite() {
+		return null;
+	}
+	
+}
