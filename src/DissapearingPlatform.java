4c5834cc64612dcdc97afd128f9e893ad28cb64d
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-09 10:32:56 -0500

    bepis

--- src/DissapearingPlatform.java
+++ src/DissapearingPlatform.java
@@ -1,16 +1,18 @@
+import java.awt.image.BufferedImage;
 
-public class DissapearingPlatform extends Platform{
+public class StationaryPlatform extends Platform{
 	
-	boolean exist;
-	
-	DissapearingPlatform(int x, int y, int width, int length) {
-		super(x, y, width, length);
+	StationaryPlatform(int x, int y) {
+		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
-
 	
-	void flip(){
-		exist = !exist;
+	StationaryPlatform(int x, int y, int width, int length) {
+		super(x, y,width,length);
+		// TODO Auto-generated constructor stub
 	}
 	
+	public static BufferedImage getSprite(){
+		return getSprite();
+	}
 }
