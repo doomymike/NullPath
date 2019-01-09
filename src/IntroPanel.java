fa8c486afce107e2e570ca96b9717e8f677e1068
bl21812 <39709270+bl21812@users.noreply.github.com>
2019-01-09 10:31:59 -0500

    some minor stuff for listener (cont to main menu)

--- src/IntroPanel.java
+++ src/IntroPanel.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class IntroPanel extends JPanel {
 
   private String buttonPressed = "";
+	private String selection = "";
 	
   public void paintComponent(Graphics g) {
 	  
@@ -15,8 +16,9 @@ public class IntroPanel extends JPanel {
     // Draw start button (rectangles right now)
     g.setColor(Color.BLUE);
     g.fillRect(400,200,200,100);
-    g.setColor(Color.BLACK);
-    g.drawRect(420,220,160,60);
+    g.setColor(Color.white);
+    g.fillRect(420,220,160,60);
+	  g.setColor(Color.BLACK);
     g.drawString("START",450,240);
     
     // Repaint
@@ -27,12 +29,16 @@ public class IntroPanel extends JPanel {
   public void setButtonPressed(String buttonPressed) {
 	  this.buttonPressed = buttonPressed;
 	  if (buttonPressed.equals("enter")) {
-		  // Go to main menu frame
+		  selection = "start";
 	  }
   }
   
   public String getButtonPressed() {
 	  return buttonPressed;
   }
+	
+	public String getSelection() {
+		return selection;
+	}
 
 }
