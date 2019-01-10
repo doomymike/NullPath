41e6ee3c357401fb2e3ec5ee2b90d09f66a4fe88
bl21812 <39709270+bl21812@users.noreply.github.com>
2019-01-10 10:59:03 -0500

    frame nav?

--- src/GameAreaFrame.java
+++ src/GameAreaFrame.java
@@ -72,6 +72,26 @@ public class GameAreaFrame extends JFrame {
 			    panelCounter = 4;
 		    }
 	    }
+	    
+	    // Go back to main menu from credits
+	    if (panelCounter == 4) {
+		    if (creditsPanel.getSelection().equals("back")) {
+			    this.remove(creditsPanel); // Remove main menu panel
+			    mainMenuPanel = new MainMenuPanel); // Add the game panel
+			    this.add(mainMenuPanel);
+			    panelCounter = 1;
+		    }
+	    }
+	    
+	    // Go back to main menu from instructions
+	    if (panelCounter == 3) {
+		    if (instructionsPanel.getSelection().equals("back")) {
+			    this.remove(instructionsPanel); // Remove main menu panel
+			    mainMenuPanel = new MainMenuPanel); // Add the game panel
+			    this.add(mainMenuPanel);
+			    panelCounter = 1;
+		    }
+	    }
 
         //this.add(mapIntegration);
         //Deprecated method - to be replaced by stage
@@ -99,17 +119,41 @@ public class GameAreaFrame extends JFrame {
             	buttonPressed = "esc";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             } else if (e.getKeyCode() == 13) { //enter pressed
             	buttonPressed = "enter";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             } else if (e.getKeyCode() == 9) { //tab pressed
             	buttonPressed = "tab";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             }
 
         }
@@ -126,22 +170,54 @@ public class GameAreaFrame extends JFrame {
             	buttonPressed = "left";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             } else if (e.getKeyCode() == 38) {//Up pressed
             	buttonPressed = "up";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             } else if (e.getKeyCode() == 39) { //Right pressed
             	buttonPressed = "right";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             } else if (e.getKeyCode() == 40) { //Down pressed
             	buttonPressed = "down";
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
-            	}
+            	} else if (panelCounter == 1) {
+			mainMenuPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 2) {
+			gamePanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 3) {
+			instructionsPanel.setButtonPressed(buttonPressed);
+		} else if (panelCounter == 4) {
+			creditsPanel.setButtonPressed(buttonPressed);
+		}
             }
             
         }
