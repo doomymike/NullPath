fb975adcb64f563f630ad0c76532243b4b427721
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-10 11:21:52 -0500

    while loop fix

--- src/GameAreaFrame.java
+++ src/GameAreaFrame.java
@@ -1,3 +1,4 @@
+package heckpath;
 //Graphics & GUI imports
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,16 +16,19 @@ import java.util.ArrayList;
 
 public class GameAreaFrame extends JFrame {
 	
+	public static void main(String[] args) {
+		new GameAreaFrame();
+	}
+	
 	private String buttonPressed = "";
 	private int panelCounter = 0;
-		// Intro - 0, Main menu - 1, Game - 2, Instructions - 3, Credits - 4, Character select - 5
+		// Intro - 0, Main menu - 1, Game - 2, Instructions - 3, Credits - 4
 	private boolean panelChange = true;
 	private IntroPanel introPanel = null;
 	private MainMenuPanel mainMenuPanel = null;
 	private GamePanel gamePanel = null;
 	private InstructionsPanel instructionsPanel = null;
 	private CreditsPanel creditsPanel = null;
-	private CharacterSelectPanel characterSelectPanel = null;
 	private MapPlacement mapIntegration = null;
 	
 	// Constructor
@@ -46,53 +50,6 @@ public class GameAreaFrame extends JFrame {
         	panelChange = false;
         }
         
-        if ((panelCounter == 0) && !panelChange) {
-        	if (introPanel.getSelection().equals("start")) {
-        		this.remove(introPanel); // Remove the intro panel
-        		mainMenuPanel = new MainMenuPanel(); // Add the main menu panel
-        		this.add(mainMenuPanel);
-        		panelCounter = 1;
-        	}
-        }
-	    // Switch to the screen chosen from main menu
-	    if (panelCounter == 1) {
-		    if (mainMenuPanel.getSelection().equals("Start Game")) {
-			    this.remove(mainMenuPanel); // Remove main menu panel
-			    characterSelectPanel = new CharacterSelectPanel(); // Add the character select panel
-			    this.add(characterSelectPanel);
-			    panelCounter = 5;
-		    } else if (mainMenuPanel.getSelection().equals("Instructions")) {
-			    this.remove(mainMenuPanel); // Remove main menu panel
-			    instructionsPanel = new InstructionsPanel(); // Add the game panel
-			    this.add(instructionsPanel);
-			    panelCounter = 3;
-		    } else if (mainMenuPanel.getSelection().equals("Credits")) {
-			    this.remove(mainMenuPanel); // Remove main menu panel
-			    creditsPanel = new CreditsPanel(); // Add the game panel
-			    this.add(creditsPanel);
-			    panelCounter = 4;
-		    }
-	    }
-	    
-	    // Go back to main menu from credits
-	    if (panelCounter == 4) {
-		    if (creditsPanel.getSelection().equals("back")) {
-			    this.remove(creditsPanel); // Remove main menu panel
-			    mainMenuPanel = new MainMenuPanel); // Add the game panel
-			    this.add(mainMenuPanel);
-			    panelCounter = 1;
-		    }
-	    }
-	    
-	    // Go back to main menu from instructions
-	    if (panelCounter == 3) {
-		    if (instructionsPanel.getSelection().equals("back")) {
-			    this.remove(instructionsPanel); // Remove main menu panel
-			    mainMenuPanel = new MainMenuPanel); // Add the game panel
-			    this.add(mainMenuPanel);
-			    panelCounter = 1;
-		    }
-	    }
 
         //this.add(mapIntegration);
         //Deprecated method - to be replaced by stage
@@ -106,18 +63,70 @@ public class GameAreaFrame extends JFrame {
 
         // Make the frame visible
         this.setVisible (true);
+        
+        while(true) {
+
+            if ((panelCounter == 0) && !panelChange) {
+            	System.out.println(introPanel.getSelection());
+            	if (introPanel.getSelection().equals("start")) {
+            		System.out.println("idk");
+            		this.remove(introPanel); // Remove the intro panel
+            		mainMenuPanel = new MainMenuPanel(); // Add the main menu panel
+            		this.add(mainMenuPanel);
+            		System.out.println("change to main menu panel");
+            		panelCounter = 1;
+            	}
+            }
+    	    // Switch to the screen chosen from main menu
+    	    if (panelCounter == 1) {
+    		    if (mainMenuPanel.getSelection().equals("Start Game")) {
+    			    this.remove(mainMenuPanel); // Remove main menu panel
+    			    gamePanel = new GamePanel(); // Add the game panel
+    			    this.add(gamePanel);
+    			    panelCounter = 2;
+    		    } else if (mainMenuPanel.getSelection().equals("Instructions")) {
+    			    this.remove(mainMenuPanel); // Remove main menu panel
+    			    instructionsPanel = new InstructionsPanel(); // Add the game panel
+    			    this.add(instructionsPanel);
+    			    panelCounter = 3;
+    		    } else if (mainMenuPanel.getSelection().equals("Credits")) {
+    			    this.remove(mainMenuPanel); // Remove main menu panel
+    			    creditsPanel = new CreditsPanel(); // Add the game panel
+    			    this.add(creditsPanel);
+    			    panelCounter = 4;
+    		    }
+    	    }
+    	    
+    	    // Go back to main menu from credits
+    	    if (panelCounter == 4) {
+    		    if (creditsPanel.getSelection().equals("back")) {
+    			    this.remove(creditsPanel); // Remove main menu panel
+    			    mainMenuPanel = new MainMenuPanel(); // Add the game panel
+    			    this.add(mainMenuPanel);
+    			    panelCounter = 1;
+    		    }
+    	    }
+    	    
+    	    // Go back to main menu from instructions
+    	    if (panelCounter == 3) {
+    		    if (instructionsPanel.getSelection().equals("back")) {
+    			    this.remove(instructionsPanel); // Remove main menu panel
+    			    mainMenuPanel = new MainMenuPanel(); // Add the game panel
+    			    this.add(mainMenuPanel);
+    			    panelCounter = 1;
+    		    }
+    	    }
+        }
 
     } // End of constructor
     
   //****** Inner Classes for KeyListener ****
 
-	//Tab doesn't work (no keycode return)
-	
     class GameKeyListener implements KeyListener{
 
         @Override
         public void keyPressed(KeyEvent e) {
-
+        	System.out.println(e.getKeyCode());
             if (e.getKeyCode() == 27) { //esc pressed
             	buttonPressed = "esc";
             	if (panelCounter == 0) {
@@ -133,6 +142,7 @@ public class GameAreaFrame extends JFrame {
 		}
             } else if (e.getKeyCode() == 10) { //enter pressed
             	buttonPressed = "enter";
+            	System.out.println("enter pressed");
             	if (panelCounter == 0) {
             		introPanel.setButtonPressed(buttonPressed);
             	} else if (panelCounter == 1) {
