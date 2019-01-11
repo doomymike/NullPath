e5b0b915f56c591ce0b185485875215bd2f44488
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-11 10:40:17 -0500

    added resources

--- src/CharacterSelectPanel.java
+++ src/CharacterSelectPanel.java
@@ -1,6 +1,18 @@
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
 public class CharacterSelectPanel extends JPanel {
 
-  public void paintComponent(Graphics g) {
-  }
+	
+	Resources resource;
+
+	public void paintComponent(Graphics g) {
+	  
+	}
+  
+	void setResources(Resources resource){
+		this.resource = resource;
+	}
 
 }
