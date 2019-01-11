c0a851844bf6087649b226e58f1cde452c523198
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-11 11:30:18 -0500

    added some end point stuff

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -15,6 +15,31 @@ public class GamePanel extends JPanel {
 		this.resource = resource;
 	}
   
+	void givePoints(Player p, int amount){
+		p.setScore(p.getScore()+amount);
+		
+	}
 	
-  
+	void death(){
+		
+	}
+	
+	void end(){
+		if(resource.getPlayers().get(0).getFinished()&&resource.getPlayers().get(1).getFinished()&&resource.getPlayers().get(2).getFinished()&&resource.getPlayers().get(3).getFinished()){
+			//too easy no point  (writing code here gottem)
+		}else if (resource.getPlayers().get(0).getFinished()||resource.getPlayers().get(1).getFinished()||resource.getPlayers().get(2).getFinished()||resource.getPlayers().get(3).getFinished()){
+			for (int i=0;i<4;i++){
+				if(!resource.getPlayers().get(i).getFinished()){
+					if (resource.getPlayers().get(i).getKilledBy().getPlacer()!= null && (resource.getPlayers().get(i).getKilledBy().getPlacer()!= resource.getPlayers().get(i))){
+						givePoints(resource.getPlayers().get(i).getKilledBy().getPlacer(),1);//trap
+					}
+				}else{
+					givePoints(resource.getPlayers().get(i),3);//finish
+				}
+			}
+		}
+			
+		
+	}
+	
 }
