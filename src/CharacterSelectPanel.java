7f6f6b4bb6134df32a304f6c28f8064c39947934
Li <072909997@EDU.YRDSB.NET>
2019-01-11 10:59:39 -0500

    fix

--- src/CharacterSelectPanel.java
+++ src/CharacterSelectPanel.java
@@ -1,18 +1,73 @@
 import java.awt.Graphics;
+import java.swing.*
 
 import javax.swing.JPanel;
 
 public class CharacterSelectPanel extends JPanel {
 
+	private int currentCharacter = 0;
+	private String buttonPressed = "";
+	private String selection = "";
+	Resources resource = null;
 	
-	Resources resource;
 
 	public void paintComponent(Graphics g) {
 	  
+		// Call the super class
+	    super.paintComponent(g);
+	    setDoubleBuffered(true);
+	    
+	    // Draw start button (rectangles right now)
+	    if (currentHover == 0) {
+	    	g.setColor(Color.RED);
+		    g.drawRect(399,199,202,102);
+	    }
+	    g.setColor(Color.BLUE);
+	    g.fillRect(400,200,200,100);
+	    g.setColor(Color.white);
+	    g.fillRect(420,220,160,60);
+		  g.setColor(Color.BLACK);
+	    g.drawString("START GAME",450,240);
+	    
+	    // Draw the instructions button
+	    if (currentHover == 1) {
+	    	g.setColor(Color.RED);
+		    g.drawRect(399,199,202,102);
+	    }
+	    g.setColor(Color.BLUE);
+	    g.fillRect(400,240,200,100);
+	    g.setColor(Color.white);
+	    g.fillRect(420,260,160,60);
+		  g.setColor(Color.BLACK);
+	    g.drawString("INSTRUCTIONS",450,280);
+	    
+	    // Draw the credits button
+	    if (currentHover == 2) {
+	    	g.setColor(Color.RED);
+		    g.drawRect(399,199,202,102);
+	    }
+	    g.setColor(Color.BLUE);
+	    g.fillRect(400,280,200,100);
+	    g.setColor(Color.white);
+	    g.fillRect(420,300,160,60);
+		  g.setColor(Color.BLACK);
+	    g.drawString("CREDITS",450,320);
+	    
+	    // Repaint
+	    repaint();
+		
 	}
   
-	void setResources(Resources resource){
+	public void setResources(Resources resource){
 		this.resource = resource;
+	}
+	
+	public void setButtonPressed(String buttonPressed) {
+		this.buttonPressed = buttonPressed;
+	}
+	
+	public String getSelection() {
+		return selection;
 	}
 
 }
