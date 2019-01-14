8387a645deb86111236cac3fb8c990ba666935b8
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-14 10:58:42 -0500

    started camera

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,7 +13,9 @@ public class GamePanel extends JPanel {
 	private String buttonPressed = "";
 	private String[][] skyFortressMapCollision = new String[36][86];
 	private BufferedImage skyFortressImage;
-
+	private int lowX,lowY,highX,HighY,ratio;
+	
+	
 	GamePanel() {
 		try {
 			skyFortressImage = ImageIO.read(new File("/resources/SkyFortress.png")); //idk if this is right pathname
@@ -61,7 +63,7 @@ public class GamePanel extends JPanel {
 	void end(){
 		if(resource.getPlayers().get(0).getFinished()&&resource.getPlayers().get(1).getFinished()&&resource.getPlayers().get(2).getFinished()&&resource.getPlayers().get(3).getFinished()){
 			//too easy no point  (writing code here gottem)
-		}else if (resource.getPlayers().get(0).getFinished()||resource.getPlayers().get(1).getFinished()||resource.getPlayers().get(2).getFinished()||resource.getPlayers().get(3).getFinished()){
+		}else if (resource.getPlayers().get(0).getFinished()||resource.getPlayers().get(1).getFinished()||resource.getPlayers().get(2).getFinished()||resource.getPlayers().get(3).getFinished()){ //if anyone finished
 			for (int i=0;i<4;i++){
 				if(!resource.getPlayers().get(i).getFinished()){
 					if (resource.getPlayers().get(i).getKilledBy().getPlacer()!= null && (resource.getPlayers().get(i).getKilledBy().getPlacer()!= resource.getPlayers().get(i))){
@@ -74,6 +76,36 @@ public class GamePanel extends JPanel {
 		}
 			
 		
+	}
+	
+	public void CameraAdjust(SimpleLinkedList<Player> players){
+		int minX = players.get(0).getCharacter().getPosition()[0];
+		int maxX = minX;
+		int minY = players.get(0).getCharacter().getPosition()[1];
+		int maxY = minY;
+		
+		for(int i =1;i<4;i++){
+			if (players.get(i).getCharacter().getPosition()[0] <minX){
+				minX = players.get(i).getCharacter().getPosition()[0];
+			}
+			if (players.get(i).getCharacter().getPosition()[0] > maxX){
+				maxX = players.get(i).getCharacter().getPosition()[0];
+			}
+			if (players.get(i).getCharacter().getPosition()[1] <minY){
+				minY = players.get(i).getCharacter().getPosition()[1];
+			}
+			if (players.get(i).getCharacter().getPosition()[0] >maxY){
+				maxY = players.get(i).getCharacter().getPosition()[1];
+			}
+			
+			minX = Math.max(minX, 0);
+			minY = Math.max(minY, 0);
+			maxX = Math.min(maxX, 860);
+			maxX = Math.min(minX, 380);
+		}
+		
+		//gonna need a ratio ops
+		//360 by 290
 	}
 
 	public void setButtonPressed(String buttonPressed) {
