380c0ce3a9dd72eb40572edb9680cbcf33bd70a3
Xynforce <brianzhang25@gmail.com>
2019-01-16 10:43:42 -0500

    MainMenuPanel Button Rehaul

--- src/MainMenuPanel.java
+++ src/MainMenuPanel.java
@@ -1,81 +1,113 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainMenuPanel extends JPanel {
-	
-	private String buttonPressed = "";
-	private String selection = "";
-	private int currentHover = 0;
-	private String[] possibleSelections = {"Start Game", "Instructions", "Credits"};
 
-	public void paintComponent(Graphics g) {
+    private int selection;
 
-		// Call the super class
-	    super.paintComponent(g);
-	    setDoubleBuffered(true);
-	    
-	    // Draw start button (rectangles right now)
-	    if (currentHover == 0) {
-	    	g.setColor(Color.RED);
-		    g.drawRect(399,199,202,102);
-	    }
-	    g.setColor(Color.BLUE);
-	    g.fillRect(400,200,200,100);
-	    g.setColor(Color.white);
-	    g.fillRect(420,220,160,60);
-		  g.setColor(Color.BLACK);
-	    g.drawString("START GAME",450,240);
-	    
-	    // Draw the instructions button
-	    if (currentHover == 1) {
-	    	g.setColor(Color.RED);
-		    g.drawRect(399,199,202,102);
-	    }
-	    g.setColor(Color.BLUE);
-	    g.fillRect(400,240,200,100);
-	    g.setColor(Color.white);
-	    g.fillRect(420,260,160,60);
-		  g.setColor(Color.BLACK);
-	    g.drawString("INSTRUCTIONS",450,280);
-	    
-	    // Draw the credits button
-	    if (currentHover == 2) {
-	    	g.setColor(Color.RED);
-		    g.drawRect(399,199,202,102);
-	    }
-	    g.setColor(Color.BLUE);
-	    g.fillRect(400,280,200,100);
-	    g.setColor(Color.white);
-	    g.fillRect(420,300,160,60);
-		  g.setColor(Color.BLACK);
-	    g.drawString("CREDITS",450,320);
-	    
-	    // Repaint
-	    repaint();
+    //Mouse Position
+    private int xPos = 0;
+    private int yPos = 0;
 
-	}
-	
-	public void setButtonPressed(String buttonPressed) {
-		this.buttonPressed = buttonPressed;
-		if (buttonPressed.equals("down") || buttonPressed.equals("tab")) {
-			if (currentHover == 2) {
-				currentHover = 0;
-			} else {
-				currentHover += 1;
-			}
-		} else if (buttonPressed.equals("up")) {
-			if (currentHover == 0) {
-				currentHover = 2;
-			} else {
-				currentHover -= 1;
-			}
-		} else if (buttonPressed.equals("enter")) {
-			selection = possibleSelections[currentHover]; // User uses enter key to select option from menu
-		}
-	}
-	
-	public String getSelection() {
-		return selection;
-	}
+    public void paintComponent(Graphics g) {
+
+        // Call the super class
+        super.paintComponent(g);
+        setDoubleBuffered(true);
+
+        //Override mouseListener methods
+        addMouseListener(new MouseAdapter(){
+            public void mousePressed(MouseEvent e){
+                xPos = e.getX();
+                yPos = e.getY();
+            }
+        });
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent evt){
+                setLocation(evt.getXOnScreen()-xPos, evt.getYOnScreen()-yPos);
+            }
+        });
+
+        this.setLayout(null);
+
+        //Setup all buttons for pedigree options (AutoDom, AutoRec, XLinkDom, XLinkRec, YLink, Samples)
+        JButton buttonOne = new JButton(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("Start.png"))));
+        JButton buttonTwo = new JButton(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("Options.png"))));
+        JButton buttonThree = new JButton(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("Quit.png"))));
+
+
+        // Draw start button (rectangles right now)
+        this.add(buttonOne);
+        buttonOne.setLocation(50,50);
+        buttonOne.setSize(128,32);
+        buttonOne.setContentAreaFilled(false);
+
+        buttonOne.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(){
+
+            }
+            public void mouseExited(){
+
+            }
+            public void mousePressed(){
+                selection = 0;
+            }
+            public void mouseClicked(){
+
+            }
+        });
+
+        // Draw options button (rectangles right now)
+        this.add(buttonTwo);
+        buttonTwo.setLocation(50,150);
+        buttonTwo.setSize(128,32);
+        buttonTwo.setContentAreaFilled(false);
+
+        buttonTwo.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(){
+
+            }
+            public void mouseExited(){
+
+            }
+            public void mousePressed(){
+                selection = 1;
+            }
+            public void mouseClicked(){
+
+            }
+        });
+
+        // Draw quit button (rectangles right now)
+        this.add(buttonThree);
+        buttonThree.setLocation(50,250);
+        buttonThree.setSize(128,32);
+        buttonThree.setContentAreaFilled(false);
+
+        buttonThree.addMouseListener(new MouseAdapter(){
+            public void mouseEntered(){
+
+            }
+            public void mouseExited(){
+
+            }
+            public void mousePressed(){
+                selection = 2;
+            }
+            public void mouseClicked(){
+
+            }
+        });
+
+        // Repaint
+        repaint();
+        setVisible(true);
+    }
+
+    public int getSelection() {
+        return selection;
+    }
 
 }
