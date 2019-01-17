1ed4ec37467a1da52e2308504edb37d528811819
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-17 11:00:08 -0500

    removed unused constructor

--- src/Spike.java
+++ src/Spike.java
@@ -7,11 +7,7 @@
 public class Spike extends ContactDamage{
 
 	//constructors
-	Spike(int x, int y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
-	}
-	
+		
 	Spike(int x, int y, int height, int width){
 		super(x,y,height,width,0);
 	}
