0627dcc346707e5fa92baee54e9a7102ab2682d2
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-17 10:58:16 -0500

    comments

--- src/Saw.java
+++ src/Saw.java
@@ -1,7 +1,12 @@
-
+/**
+ * Saw.java
+ * @author Brian Li, James Liang, Michael Oren, Brian Zhang
+ * Class for saw object
+ *
+ */
 public class Saw extends ContactDamage{
 	
-	
+	//constructor
 	Saw(int x, int y, int radius) {
 		super(x, y,0,0,radius);
 		// TODO Auto-generated constructor stub
