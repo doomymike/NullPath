4375f1e6393fd5849d5e9230e460ffd73e159427
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-17 11:15:56 -0500

    commenting

--- src/Item.java
+++ src/Item.java
@@ -1,12 +1,20 @@
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-
+/**
+ * Item.java
+ * @author Brian Li, James Liang, Michael Oren, Brian Zhang
+ * Class for all items
+ *
+ */
 public class Item {
 	private int x,y,height, width, radius;
 	private double dx,dy;
 	private boolean affectGravity = true;
 	private ArrayList<Integer> charIntersect = new ArrayList<Integer>(); //Every item contains a reference of which characters it's intersecting with
 	private Player placer;
+	private BufferedImage image;
 	
+	//physics functions
 	public void printCharLen() {
 		System.out.println(charIntersect.size());
 	}
@@ -32,6 +40,7 @@ public class Item {
 		return false;
 	}
 	
+	//getters and setters
 	public int getX(){
 		return x;
 	}
@@ -79,6 +88,23 @@ public class Item {
 		dy = newDy;
 	}
 	
+	public Player getPlacer() {
+		return placer;
+	}
+
+	public void setPlacer(Player placer) {
+		this.placer = placer;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+	
+	//constructors
 	Item(int x, int y, int height, int width, boolean affectGravity){
 		this.x = x;
 		this.y = y;
@@ -124,13 +150,4 @@ public class Item {
 		this.x = x;
 		this.y = y;
 	}
-
-	public Player getPlacer() {
-		return placer;
-	}
-
-	public void setPlacer(Player placer) {
-		this.placer = placer;
-	}
-	
 }
