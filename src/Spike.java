313a106a922e9e40a93725ed04d6a32f86cc1d88
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-17 10:56:56 -0500

    commenting

--- src/Spike.java
+++ src/Spike.java
@@ -1,6 +1,12 @@
-
+/**
+ * Spike.java
+ * @author Brian Li, James Liang, Michael Oren, Brian Zhang
+ * Class for spike object
+ *
+ */
 public class Spike extends ContactDamage{
 
+	//constructors
 	Spike(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
