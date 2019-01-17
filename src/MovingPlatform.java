7127c79fbd4d6a2c2e185876e972ba2cbc20466d
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-17 11:10:25 -0500

    commenting

--- src/MovingPlatform.java
+++ src/MovingPlatform.java
@@ -1,11 +1,18 @@
-//
+
 
 import java.awt.image.BufferedImage;
 
+/**
+ * MovingPlatform.java
+ * @author Brian Li, James Liang, Michael Oren, Brian Zhang
+ * Class for moving platforms
+ *
+ */
 public class MovingPlatform extends Platform{
 	
 	int x2,y2,x3,y3	,direction,speed; //x2y2 are bottom left x3y3 are top right
 	
+	//constructors
 	MovingPlatform(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
@@ -26,6 +33,7 @@ public class MovingPlatform extends Platform{
 		return getSprite();
 	}
 	
+	//checks if platform is within path and moves along it
 	public void move(){
 		if(direction == 0){ //left
 			if(this.getX() <=x2){
