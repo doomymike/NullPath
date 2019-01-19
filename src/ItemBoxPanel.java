04d70a0a9de792b45a9af736009f914022a8e4a0
Xynforce <brianzhang25@gmail.com>
2019-01-19 12:20:54 -0500

    Basic display box

--- src/ItemBoxPanel.java
+++ src/ItemBoxPanel.java
@@ -1,14 +1,34 @@
+import java.awt.*;
+import javax.swing.*;
 
-public class ItemBoxPanel {
-	
-	private ItemBox box;
-	
-	public ItemBoxPanel(){
-		box = new ItemBox(4); //should be num players
-		for(int i =0;i<box.getItems().size();i++){
-			//draw the boi on the screen
-		}
-	}
-	
-	
+public class ItemBoxPanel extends JPanel{
+
+    private ItemBox box;
+
+    public void paintComponent(Graphics g) {
+
+        JLabel select = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("ItemBox.gif"))));
+
+        // Call the super class
+        super.paintComponent(g);
+        setDoubleBuffered(true);
+
+        //Box
+        this.add(select);
+        select.setLocation(50,0);
+        select.setSize(700,700);
+
+        // Repaint
+        repaint();
+
+    }
+
+    public ItemBoxPanel(){
+        box = new ItemBox(4); //should be num players
+        for(int i =0;i<box.getItems().size();i++){
+            //draw the boi on the screen
+        }
+    }
+
+
 }
