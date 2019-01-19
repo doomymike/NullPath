fbf1b70a2641e39b762672e4b128d7506052b1bd
Xynforce <brianzhang25@gmail.com>
2019-01-19 10:44:27 -0500

    New labels for instructions

--- src/InstructionsPanel.java
+++ src/InstructionsPanel.java
@@ -3,45 +3,39 @@ import javax.swing.*;
 
 public class InstructionsPanel extends JPanel {
 
-  private String buttonPressed = "";
-	private String selection = "";
-	
-  public void paintComponent(Graphics g) {
-	  
-	// Call the super class
-    super.paintComponent(g);
-    setDoubleBuffered(true);
-    
-    // Draw credits text (filler right now)
-	  g.setColor(Color.BLACK);
-    g.drawString("Instructions",450,240);
-    
-    // Draw back button (top left)
-    g.setColor(Color.BLUE);
-    g.fillRect(20,20,100,50);
-    g.setColor(Color.white);
-    g.fillRect(30,30,80,30);
-	  g.setColor(Color.BLACK);
-    g.drawString("BACK",50,50);
-    
-    // Repaint
-    repaint();
-	  
-  }
-  
-  public void setButtonPressed(String buttonPressed) {
-	  this.buttonPressed = buttonPressed;
-	  if (buttonPressed.equals("enter")) {
-		  selection = "back";
-	  }
-  }
-  
-  public String getButtonPressed() {
-	  return buttonPressed;
-  }
-	
-	public String getSelection() {
-		return selection;
-	}
+    private String buttonPressed = "";
+    private String selection = "";
+
+    public void paintComponent(Graphics g) {
+
+        JLabel flow = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("WaterFlow.gif"))));
+        JLabel instructions = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("Instructions.png"))));
+        JLabel copyright = new JLabel(new ImageIcon(Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("Copyright.png"))));
+
+        // Call the super class
+        super.paintComponent(g);
+        setDoubleBuffered(true);
+
+        //Instructions L
+        this.add(instructions);
+        instructions.setLocation(100,25);
+        instructions.setSize(800,514);
+
+        this.add(copyright);
+        copyright.setLocation(100,600);
+        copyright.setSize(800,57);
+
+        this.add(flow);
+        flow.setLocation(0,-100);
+        flow.setSize(2000,1020);
+
+        // Repaint
+        repaint();
+
+    }
+
+    public String getSelection() {
+        return selection;
+    }
 
 }
