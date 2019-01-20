dc9c32fd00f0bf374c22ea21975cd388fe90eb55
bl21812 <39709270+bl21812@users.noreply.github.com>
2019-01-19 19:36:31 -0500

    some comments

--- src/Resources.java
+++ src/Resources.java
@@ -1,19 +1,27 @@
-//0 = left, 
+/**
+ * Resources
+ * Object that contains players, stages, and characters for the game
+ * Authors: Brian Li, James Liang, Michael Oren, Brian Zhang
+ * Date: January 21, 2019
+ */
 
+// Java imports
 import java.io.*;
 
 public class Resources {
 
-	private static SimpleLinkedList<Stage> stages = new SimpleLinkedList<Stage>();
-	//Add all stages to list
+	private static SimpleLinkedList<Stage> stages = new SimpleLinkedList<Stage>(); // List of stages
 	
-	private SimpleLinkedList<Character> characters = new SimpleLinkedList<Character>();
-	//Initialize character objects (will add players when players are assigned)
+	private SimpleLinkedList<Character> characters = new SimpleLinkedList<Character>(); // List of characters
 	
-	private Stage currentStage;
+	private Stage currentStage; // Stores the current stage being played on
 	
-	private SimpleLinkedList<Player> players = new SimpleLinkedList<Player>();
-	
+	private SimpleLinkedList<Player> players = new SimpleLinkedList<Player>(); // List of players
+
+	/**
+	 * Resources
+	 * Constructor that makes a resources object with characters and stages in the game
+	 */
 	Resources() {
 		
 		//Initialize character objects (will add players when players are assigned)
@@ -29,54 +37,99 @@ public class Resources {
 		//Add all stages to list
 		stages.add(new Stage("SkyFortress"));
 		
-	}
-	
+	} // End of constructor
+
+	/**
+	 * getStages
+	 * Returns list of stages
+	 * @return SimpleLInkedList<Stage> of stages
+	 */
 	public static SimpleLinkedList<Stage> getStages() {
 		return stages;
-	}
-	
+	} // End of getStages
+
+	/**
+	 * getRandomStage
+	 * Returns a random stage from list of stages
+	 * @return Random Stage object
+	 */
 	public static Stage getRandomStage() {
 		if (stages.size() != 0) {
 			int rand = (int)(Math.round(Math.random()*(stages.size()-1)));
-			return stages.get(rand);
+			return stages.get(rand); // Choose random stage
 		}
 		return null;
-	}
-	
+	} // End of getRandomStage
+
+	/**
+	 * getCharacters
+	 * Returns list of characters
+	 * @return SimpleLinkedList<Character> of characters
+	 */
 	public SimpleLinkedList<Character> getCharacters() {
 		return characters;
-	}
-	
+	} // End of getCharacters
+
+	/**
+	 * getRandomCharacter
+	 * Returns a random character from list of characters
+	 * @return Random character object
+	 */
 	public Character getRandomCharacter() {
 		if (characters.size() != 0) {
 			int rand = (int)(Math.round(Math.random()*(characters.size()-1)));
-			return characters.get(rand);
+			return characters.get(rand); // Choose random character
 		}
 		return null;
-	}
-	
+	} // End of getRandomCharacter
+
+	/**
+	 * setCurrentStage
+	 * Sets the active stage being played on
+	 * @param stage, the stage to be set to the active stage
+	 */
 	public void setCurrentStage(Stage stage) {
 		currentStage = stage;
-	}
-	
+	} // End of setCurrentStage
+
+	/**
+	 * getCurrentStage
+	 * Returns the active stage
+	 * @return Stage object representing the current stage played on
+	 */
 	public Stage getCurrentStage() {
 		return currentStage;
-	}
-	
+	} // End of getCurrentStage
+
+	/**
+	 * getPlayers
+	 * Returns list of players
+	 * @return SimpleLinkedList<Player> of players
+	 */
 	public SimpleLinkedList<Player> getPlayers() {
 		return players;
-	}
-	
+	} // End of getPlayers
+
+	/**
+	 * addPlayer
+	 * Adds player to list of players
+	 * @param name, String representing name of player added
+	 */
 	public void addPlayer(String name) {
 		players.add(new Player(name));
-	}
-	
+	} // End of addPlayer
+
+	/**
+	 * removePlayer
+	 * Removes player from list of players
+	 * @param name, String representing name of player to be removed
+	 */
 	public void removePlayer(String name) {
 		for (int i = 0; i < players.size(); i++) {
 			if (players.get(i).getName().equals(name)) {
 				players.remove(i);
 			}
 		}
-	}
+	} // End of removePlayer
 	
-}
+} // End of class
--- src/SimpleQueue.java
+++ src/SimpleQueue.java
@@ -1,13 +1,29 @@
+/**
+ * SimpleQueue
+ * Queue object that stores nodes containing generic items
+ * Authors: Brian Li, James Liang, Michael Oren, Brian Zhang
+ * Date: January 21, 2019
+ */
+
 public class SimpleQueue<T> {
 
-    Node<T> head;
-    Node<T> tail;
+    Node<T> head; //head node (node to be dequeued)
+    Node<T> tail; // tail node (node last enqueued)
 
+    /**
+     * SimpleQueue
+     * Constructor that makes an empty queue
+     */
     public SimpleQueue() {
         head = null;
         tail = null;
-    }
+    } // End of constructor
 
+    /**
+     * offer
+     * Enqueues an item (into new tail node)
+     * @param item, the generic item to be enqueued
+     */
     public void offer(T item) {
         if (tail == null) {
             head = new Node<>(item, null);
@@ -16,8 +32,13 @@ public class SimpleQueue<T> {
             tail.setNext(new Node<>(item, null));
             tail = tail.getNext();
         }
-    }
+    } // End of offer
 
+    /**
+     * poll
+     * Dequeues an item (item in head node)
+     * @return generic item in head node
+     */
     public T poll() {
         if (head == null) {
             return null;
@@ -26,27 +47,28 @@ public class SimpleQueue<T> {
             head = head.getNext();
             return temp;
         }
-    }
+    } // End of poll
 
+    /**
+     * peek
+     * Returns the item held in the head node
+     * @return generic item held in head node
+     */
     public T peek() {
         if (head == null) {
             return null;
         } else {
             return head.getItem();
         }
-    }
+    } // End of peek
 
+    /**
+     * clear
+     * Clears the queue of all items
+     */
     public void clear() {
         head = null;
         tail = null;
-    }
+    } // End of clear
 
-    public boolean isEmpty() {
-        if (head == null) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-}
+} // End of class
