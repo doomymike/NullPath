6dbeb3c33287750f158d556b802c3171297fe0f1
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-20 15:32:06 -0500

    spite

--- src/Arrow.java
+++ src/Arrow.java
@@ -7,6 +7,12 @@ public class Arrow extends Projectile{
 	
 	public Arrow(int x, int y, double[] speed, String orientation, int tag) {
 		super(x, y, height, width, speed, false, tag);
+		try {
+			this.setImage(ImageIO.read(new File("C:\\Users\\Michael\\eclipse-workspace\\ughPath\\src/Arrow.png")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if (orientation.equals("V")) {
 			setHeight(width);
 			setWidth(height);
