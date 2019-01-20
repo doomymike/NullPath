b461cd122b8a1bcdefd95197bd622068a09e777a
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-20 12:56:21 -0500

    sprite support

--- src/Saw.java
+++ src/Saw.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 /**
  * Saw.java
  * @author Brian Li, James Liang, Michael Oren, Brian Zhang
@@ -10,6 +15,18 @@ public class Saw extends ContactDamage{
 	Saw(int x, int y, int radius) {
 		super(x, y,0,0,radius);
 		// TODO Auto-generated constructor stub
+		
+		try {
+			
+			for (int i = 1;i<25;i++){
+				this.addSprite(ImageIO.read(new File("C:\\Users\\Michael\\eclipse-workspace\\ughPath\\src/Saw"+i+".png")));
+			}
+			this.setImage(this.getSprites().get(0));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
