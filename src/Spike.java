9c40e7d24e2d6692afb198501ba1772127f2445a
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-20 12:56:54 -0500

    sprite support

--- src/Spike.java
+++ src/Spike.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 /**
  * Spike.java
  * @author Brian Li, James Liang, Michael Oren, Brian Zhang
@@ -10,6 +15,12 @@ public class Spike extends ContactDamage{
 		
 	Spike(int x, int y, int height, int width){
 		super(x,y,height,width,0);
+		try {
+			this.setImage(ImageIO.read(new File("C:\\Users\\Michael\\eclipse-workspace\\ughPath\\src/SpikeBall.png")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
