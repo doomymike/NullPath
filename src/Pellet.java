6cf7d0418af262857c90b9f05c968e63da9827e0
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-20 17:30:18 -0500

    sprite

--- src/Pellet.java
+++ src/Pellet.java
@@ -1,8 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Pellet extends Projectile{
 	
 	public Pellet(int x, int y, int radius, double[] speed, boolean affectGravity, int tag) {
 		super(x, y, radius, speed, affectGravity, tag);
+		try {
+			this.setImage(ImageIO.read(new File("resources/Cannonball.png")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
