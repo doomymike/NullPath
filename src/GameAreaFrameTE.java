9ad5a098a11a40151b68588135c11191109dd0e0
Xynforce <brianzhang25@gmail.com>
2019-01-21 19:35:07 -0500

    Fixed keylistener boi

--- src/GameAreaFrameTE.java
+++ src/GameAreaFrameTE.java
@@ -39,7 +39,7 @@ public class GameAreaFrameTE extends JFrame implements KeyListener {
     private MapPlacement mapIntegration = null;
     private testFrameFT gameB = null;
     private itemMove itemM = null;
-    
+
     GameClient client = new GameClient();
 
     private Player player = null; // Player that is running the game
@@ -87,24 +87,24 @@ public class GameAreaFrameTE extends JFrame implements KeyListener {
         // Connect to server
         //new GameClient().go();
         try {
-			physicsEngine = new PhysicsEngine("contact");
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+            physicsEngine = new PhysicsEngine("contact");
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
 
         // Initialize resources object (with characters + stage)
         resources = new Resources();
         resources.setCurrentStage(resources.getStages().get(0));
-        
+
         // Set screen size
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         this.setSize(1720, 760); //Filler values
         this.setResizable(false);
 
         // Set up the intro panel if start
-        /*
+
         if ((panelCounter == 0) && panelChange) {
             introPanel = new IntroPanel();
             introPanel.addKeyListener(introPanel);
@@ -113,38 +113,42 @@ public class GameAreaFrameTE extends JFrame implements KeyListener {
             this.getContentPane().add(introPanel);
             panelChange = false;
         }
-        */
 
+
+        
         itemM = new itemMove(resources, physicsEngine);
         itemM.setFocusable(true);
         this.getContentPane().add(itemM);
-                
+
+
         this.requestFocusInWindow();
 
         // Make the frame visible
         this.setVisible(true);
-        
+
         boolean bad = true;
+
         
         while (bad) {
-        	System.out.print("");
-	        if (itemM.checkDone()) {
-	        	this.getContentPane().remove(itemM);
-	        	try {
-	    			gameB = new testFrameFT(resources);
-	    		} catch (IOException e1) {
-	    			// TODO Auto-generated catch block
-	    			e1.printStackTrace();
-	    		}
-	            itemM.setDone(false);
-	        	gameB.setFocusable(true);
-	        	gameB.requestFocus();
-	            this.getContentPane().add(gameB);
-	            this.revalidate();
-	            this.repaint();
-	        }
+            System.out.print("");
+            if (itemM.checkDone()) {
+                this.getContentPane().remove(itemM);
+                try {
+                    gameB = new testFrameFT(resources);
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                itemM.setDone(false);
+                gameB.setFocusable(true);
+                gameB.requestFocus();
+                this.getContentPane().add(gameB);
+                this.revalidate();
+                this.repaint();
+            }
         }
         
+
         //this.add(mapIntegration);
         //Deprecated method - to be replaced by stage
 
@@ -162,114 +166,9 @@ public class GameAreaFrameTE extends JFrame implements KeyListener {
         //e.printStackTrace();
         //}
 
+    }
 
-        //Switch to Main Menu from Intro
-        while (panelCounter == 0) {
-            if ((panelCounter == 0) && !panelChange) {
-                if (introPanel.getNext()) {
-                    this.getContentPane().remove(introPanel); // Remove the intro panel
-                    mainMenuPanel = new MainMenuPanel(); // Add the main menu panel
-                    this.getContentPane().add(mainMenuPanel);
-                    System.out.println("change to main menu panel");
-                    panelCounter = 1;   //1 IS MAIN MENU
-                    this.revalidate();
-                    repaint();
-                }
-            }
-        }
-
-        // Switch to the screen chosen from main menu
-        while (panelCounter == 1) {
-            if (mainMenuPanel.getSelection() == 0) {
-                new ClientLogin(client, resources); // Open client login above main menu
-                panelCounter = 7;
-            } else if (mainMenuPanel.getSelection() == 1) {
-                this.getContentPane().remove(mainMenuPanel); // Remove main menu panel
-                instructionsPanel = new InstructionsPanel(); // Add the game panel
-                this.getContentPane().add(instructionsPanel);
-                panelCounter = 2;   //2 IS INSTRUCTIONS
-                this.revalidate();
-                repaint();
-            } else if (mainMenuPanel.getSelection() == 2) {
-                System.out.println("Program Ended");
-                this.dispose();
-                System.exit(0);
-            }
-        }
-
-        while (panelCounter == 7) { // On login screen
-            if (resources.getPlayers().size() == 4) { // Check that all 4 player objects have been made in resources
-                for (int i = 0; i < 4; i++) {
-                    if (resources.getPlayers().get(i).getName().equals(client.getUsername())) {
-                        player = resources.getPlayers().get(i); // Set the player corresponding to this specific instance of the game
-                    }
-                }
-                this.getContentPane().remove(mainMenuPanel); // Remove main menu panel
-                characterSelectPanel = new CharacterSelectPanel(); // Add the game panel
-                this.getContentPane().add(characterSelectPanel);
-                characterSelectPanel.setResources(resources); // Pass in reference to resources object (so players can be assigned characters)
-                characterSelectPanel.setClient(client);
-                panelCounter = 3;   //3 IS GAME PANEL
-                this.revalidate();
-                repaint();
-            }
-        }
-
-        // Go back to main menu from credits
-        while (panelCounter == 6) {
-            if (creditsPanel.getSelection().equals("back")) {
-                this.getContentPane().remove(creditsPanel); // Remove main menu panel
-                mainMenuPanel = new MainMenuPanel(); // Add the game panel
-                this.getContentPane().add(mainMenuPanel);
-                panelCounter = 1;   //1 IS MAIN MENU
-                this.revalidate();
-                repaint();
-            }
-        }
-
-
-            // Character select screen (back button and character selection)
-            while (panelCounter == 3) {
-
-                try {
-                    // Character assignment
-                    if (characterSelectPanel.getSelection().equals("blue")) {
-                        player.setCharacter(new Character("blue"));
-                        // add to resources object?
-                    } else if (characterSelectPanel.getSelection().equals("green")) {
-                        player.setCharacter(new Character("green"));
-                    } else if (characterSelectPanel.getSelection().equals("red")) {
-                        player.setCharacter(new Character("green"));
-                    } else if (characterSelectPanel.getSelection().equals("yellow")) {
-                        player.setCharacter(new Character("green"));
-                    }
-                } catch (IOException e) {
-                    // Shouldn't ever run (unless images are improperly labelled) - empty catch
-                }
-
-                // Change to game panel or back to menu panel
-                if ((characterSelectPanel.getSelection().equals("blue")) || (characterSelectPanel.getSelection().equals("blue")) || (characterSelectPanel.getSelection().equals("blue")) || (characterSelectPanel.getSelection().equals("blue"))) {
-                    this.getContentPane().remove(characterSelectPanel);
-                    gamePanel = new GamePanel();
-                    this.getContentPane().add(gamePanel);
-                    gamePanel.setResources(resources); // Pass in reference to resources object
-                    panelCounter = 4; //4 IS GAME PANEL
-                    this.revalidate();
-                    repaint();
-                } else if (characterSelectPanel.getSelection().equals("back")) {
-                    this.getContentPane().remove(characterSelectPanel);
-                    mainMenuPanel = new MainMenuPanel();
-                    this.getContentPane().add(mainMenuPanel);
-                    panelCounter = 1; //1 IS MAIN MENU
-                    this.revalidate();
-                    repaint();
-                }
-
-            }
-
-        }
-
-     // End of constructor
+    // End of constructor
 
     //****** Inner Classes for KeyListener ****
 
@@ -285,7 +184,7 @@ public class GameAreaFrameTE extends JFrame implements KeyListener {
                 } else if (panelCounter == 2){
                     System.out.println("Back from Instructions");
                 }else if (panelCounter == 3) {
-                        System.out.println("Back from Character Select");
+                    System.out.println("Back from Character Select");
                 } else if (panelCounter == 6) {
                     System.out.println("Back from Credits");
                 }
@@ -362,5 +261,100 @@ public class GameAreaFrameTE extends JFrame implements KeyListener {
         }
 
     } //End of inner class
+
+    //START OF LOOP METHOD
+    public void gameLoop(){
+
+        //Switch to Main Menu from Intro
+        if ((panelCounter == 0) && !panelChange) {
+            if (introPanel.getNext()) {
+                this.getContentPane().remove(introPanel); // Remove the intro panel
+                mainMenuPanel = new MainMenuPanel(); // Add the main menu panel
+                this.getContentPane().add(mainMenuPanel);
+                System.out.println("change to main menu panel");
+                panelCounter = 1;   //1 IS MAIN MENU
+                this.revalidate();
+                repaint();
+            }
+        }else if (panelCounter == 1) {
+            if (mainMenuPanel.getSelection() == 0) {
+                client = new GameClient();
+                new ClientLogin(client, resources); // Open client login above main menu
+                panelCounter = 7;
+            } else if (mainMenuPanel.getSelection() == 1) {
+                this.getContentPane().remove(mainMenuPanel); // Remove main menu panel
+                instructionsPanel = new InstructionsPanel(); // Add the game panel
+                this.getContentPane().add(instructionsPanel);
+                panelCounter = 2;   //2 IS INSTRUCTIONS
+                this.revalidate();
+                repaint();
+            } else if (mainMenuPanel.getSelection() == 2) {
+                System.out.println("Program Ended");
+                this.dispose();
+                System.exit(0);
+            }
+        }else if (panelCounter == 7) { // On login screen
+            if (resources.getPlayers().size() == 4) { // Check that all 4 player objects have been made in resources
+                for (int i = 0; i < 4; i++) {
+                    if (resources.getPlayers().get(i).getName().equals(client.getUsername())) {
+                        player = resources.getPlayers().get(i); // Set the player corresponding to this specific instance of the game
+                    }
+                }
+                this.getContentPane().remove(mainMenuPanel); // Remove main menu panel
+                characterSelectPanel = new CharacterSelectPanel(); // Add the game panel
+                this.getContentPane().add(characterSelectPanel);
+                characterSelectPanel.setResources(resources); // Pass in reference to resources object (so players can be assigned characters)
+                characterSelectPanel.setClient(client);
+                panelCounter = 3;   //3 IS GAME PANEL
+                this.revalidate();
+                repaint();
+            }
+        }else if (panelCounter == 6) {
+            if (creditsPanel.getSelection().equals("back")) {
+                this.getContentPane().remove(creditsPanel); // Remove main menu panel
+                mainMenuPanel = new MainMenuPanel(); // Add the game panel
+                this.getContentPane().add(mainMenuPanel);
+                panelCounter = 1;   //1 IS MAIN MENU
+                this.revalidate();
+                repaint();
+            }
+        }else if (panelCounter == 3) {
+            try {
+                // Character assignment
+                if (characterSelectPanel.getSelection().equals("blue")) {
+                    player.setCharacter(new Character("blue"));
+                    // add to resources object?
+                } else if (characterSelectPanel.getSelection().equals("green")) {
+                    player.setCharacter(new Character("green"));
+                } else if (characterSelectPanel.getSelection().equals("red")) {
+                    player.setCharacter(new Character("green"));
+                } else if (characterSelectPanel.getSelection().equals("yellow")) {
+                    player.setCharacter(new Character("green"));
+                }
+            } catch (IOException e) {
+                // Shouldn't ever run (unless images are improperly labelled) - empty catch
+            }
+
+            // Change to game panel or back to menu panel
+            if ((characterSelectPanel.getSelection().equals("blue")) || (characterSelectPanel.getSelection().equals("blue")) || (characterSelectPanel.getSelection().equals("blue")) || (characterSelectPanel.getSelection().equals("blue"))) {
+                this.getContentPane().remove(characterSelectPanel);
+                gamePanel = new GamePanel();
+                this.getContentPane().add(gamePanel);
+                gamePanel.setResources(resources); // Pass in reference to resources object
+                panelCounter = 4; //4 IS GAME PANEL
+                this.revalidate();
+                repaint();
+            } else if (characterSelectPanel.getSelection().equals("back")) {
+                this.getContentPane().remove(characterSelectPanel);
+                mainMenuPanel = new MainMenuPanel();
+                this.getContentPane().add(mainMenuPanel);
+                panelCounter = 1; //1 IS MAIN MENU
+                this.revalidate();
+                repaint();
+            }
+
+        }
+
+    }
 
 }
