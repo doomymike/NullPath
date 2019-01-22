2194f0d5f681d26d67c57ef106641c0960f35232
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-21 21:45:01 -0500

    static

--- src/MovingPlatform.java
+++ src/MovingPlatform.java
@@ -5,18 +5,18 @@ import java.awt.image.BufferedImage;
 public class MovingPlatform extends Platform{
 	
 	int x2,y2,x3,y3,direction; //Direction: 0 = start -> end, 1 = end -> start
-	int maxTravelD = 100;
-
-	MovingPlatform(int x, int y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
-	}
+	private static int maxTravelD = 100;
 	
-	MovingPlatform(int x, int y, int height, int width) {
+	private static int height = 40;
+	private static int width = 130;
+	
+	
+	
+	MovingPlatform(int x, int y) {
 		super(x, y,height,width);
 	}
 
-	MovingPlatform(int x, int y, int height, int width,int x2,int y2, double[] vel, int direction) {
+	MovingPlatform(int x, int y,int x2,int y2, double[] vel, int direction) {
 		super(x, y,height,width, vel);
 		this.x2 = x;
 		this.y2 = y;
@@ -40,6 +40,14 @@ public class MovingPlatform extends Platform{
 	
 	public static BufferedImage getSprite(){
 		return getSprite();
+	}
+	
+	public static int getH() {
+		return height;
+	}
+	
+	public static int getW() {
+		return width;
 	}
 	
 }
