b52cb787acdd04a2fce9ee3ecf0f435e56164205
JL321 <james.liangyy@gmail.com>
2019-01-22 11:28:44 -0500

    c

--- src/MovingPlatform.java
+++ src/MovingPlatform.java
@@ -1,4 +1,8 @@
-//
+/* [MovingPlatform.java]
+ * Class containing relevant info for moving platform
+ * Author: James, Michael
+ * Date: 01/21/19
+ */
 
 import java.awt.image.BufferedImage;
 
@@ -10,12 +14,32 @@ public class MovingPlatform extends Platform{
 	private static int height = 40;
 	private static int width = 130;
 	
-	
+	/**
+	 * MovingPlatform
+	 * 
+	 * Constructor for moving platform
+	 * 
+	 * @param x
+	 * @param y
+	 */
 	
 	MovingPlatform(int x, int y) {
 		super(x, y,height,width);
 	}
 
+	/**
+	 * MovingPlatform
+	 * 
+	 * Alt constructor
+	 * 
+	 * @param x
+	 * @param y
+	 * @param x2
+	 * @param y2
+	 * @param vel
+	 * @param direction
+	 */
+	
 	MovingPlatform(int x, int y,int x2,int y2, double[] vel, int direction) {
 		super(x, y,height,width, vel);
 		this.x2 = x;
@@ -26,25 +50,73 @@ public class MovingPlatform extends Platform{
 		// TODO Auto-generated constructor stub
 	}
 	
+	/**
+	 * getStartPos
+	 * 
+	 * Returns starting position of moving platform
+	 * 
+	 * @return
+	 */
+	
 	public int[] getStartPos() {
 		return new int[] {x2, y2};
 	}
+	
+	/**
+	 * getEndPos
+	 * 
+	 * Returns ending position of moving platform
+	 * 
+	 * @return
+	 */
 	
 	public int[] getEndPos() {
 		return new int[] {x3, y3};
 	}
 	
+	/**
+	 * getDirection 
+	 * 
+	 * Get direction
+	 * 
+	 * @return
+	 */
+	
 	public int getDirection() {
 		return direction;
 	}
+	
+	/**
+	 * getSprite
+	 * 
+	 * Returns associated sprite
+	 * 
+	 * @return
+	 */
 	
 	public static BufferedImage getSprite(){
 		return getSprite();
 	}
 	
+	/**
+	 * getH
+	 * 
+	 * Returns height
+	 * 
+	 * @return
+	 */
+	
 	public static int getH() {
 		return height;
 	}
+	
+	/**
+	 * getW
+	 * 
+	 * Returns width
+	 * 
+	 * @return
+	 */
 	
 	public static int getW() {
 		return width;
