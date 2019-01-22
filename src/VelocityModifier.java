2cd00bc29f663650bd5ec898f81b3894197254c4
bl21812 <39709270+bl21812@users.noreply.github.com>
2019-01-22 00:00:34 -0500

    Velocity modifier commented

--- src/VelocityModifier.java
+++ src/VelocityModifier.java
@@ -1,8 +1,14 @@
+/**
+ * [VelocityModifier.java]
+ * Object that modifies the velocity of a character in NullPath game
+ * Authors: Brian Li, James Liang, Michael Oren, Brian Zhang
+ * January 22, 2019
+ */
 
 public class VelocityModifier extends Item{
 
-	private int dirMarker;
-	private int xyDir;
+	private int dirMarker; //Stores direction
+	private int xyDir; //Marks as lateral/vertical
 	
 	//Dictionary (dirMarker):
 	//0 = Speed (direct abs influence on player speed)s
@@ -12,23 +18,47 @@ public class VelocityModifier extends Item{
 	//Dictionary (xyDir):
 	//0 = Lateral
 	//1 = Vertical
-	
-	public int getDirection() {
-		return dirMarker;
-	}
-	
-	public int getDirectionXY() {
-		return xyDir;
-	}
-	
-	public double[] getSpeed() {
-		return null;
-	}
-	
+
+	/**
+	 * VelocityModifier
+	 * @param x, int representing the x coordinate
+	 * @param y, int representing the y coordinate
+	 * @param height, int representing the height of item
+	 * @param width, int representing the width of item
+	 * @param direction, int representing direction (+/-) of velocity modification
+	 * @param direction2, int representing lateral/vertical marker
+	 */
 	public VelocityModifier(int x, int y, int height, int width, int direction, int direction2) {
 		super (x, y, height, width);
 		this.dirMarker = direction;
 		this.xyDir = direction2;
-	}
+	} //End of constructor
 
-}
+	/**
+	 * getDirection
+	 * Returns the direction
+	 * @return int dirMarker, an int representing the direction of velocity modification
+	 */
+	public int getDirection() {
+		return dirMarker;
+	} //End of getDirection
+
+	/**
+	 * getDirectionXY
+	 * Returns whether x or y direction
+	 * @return int xyDir, an int which marks whether velocity modification is lateral or vertical
+	 */
+	public int getDirectionXY() {
+		return xyDir;
+	} //End of getDirectionXY
+
+	/**
+	 * getSpeed
+	 * Returns the speed of velocity modification
+	 * @return double[], representing speed of velocity modification
+	 */
+	public double[] getSpeed() {
+		return null;
+	} //End of getSpeed
+
+} //End of class
