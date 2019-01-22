ef18f9390d2a1286cbee110d20a5fce5db38e210
doomymike <34379835+doomymike@users.noreply.github.com>
2019-01-21 21:45:40 -0500

    static

--- src/Arrow.java
+++ src/Arrow.java
@@ -5,23 +5,30 @@ import javax.imageio.ImageIO;
 
 public class Arrow extends Projectile{
 
-	static int width = 30;
-	static int height = 15;
+	private static int width = 30;
+	private static int height = 15;
 	private String dir;
 	
-	public Arrow(int x, int y, double[] speed, int tag, String dir) {
+	public Arrow(int x, int y, double[] speed, String orientation, int tag) {
 		super(x, y, height, width, speed, false, tag);
-		this.dir = dir;
 		try {
 			this.setImage(ImageIO.read(new File("resources/Arrow.png")));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if (dir.equals("V")) {
+		if (orientation.equals("V")) {
 			setHeight(width);
 			setWidth(height);
 		}
+	}
+	
+	public static int getH() {
+		return height;
+	}
+	
+	public static int getW() {
+		return width;
 	}
 	
 }
