56fc8b47fe569b94933555df7296e16b98d2edf8
Xynforce <brianzhang25@gmail.com>
2019-01-22 11:27:25 -0500

    c

--- src/StartGame.java
+++ src/StartGame.java
@@ -1,7 +1,15 @@
+/*[StartGame.java]
+  Main class for running game operations
+  Authors: Brian Li, Brian Zhang, James Liang, Michael Oren
+  Date: January 21, 2019
+*/
+
 public class StartGame{
 
+	//Main run
 	public static void main(String [] args) {
 		GameAreaFrameTE game = new GameAreaFrameTE();
+		//Game loop execution
 		while(true) {
 			game.gameLoop();
 			game.roundLoop();
