37d7bd5916deab06a05b204d7ffc2bb69c3f2352
Xynforce <brianzhang25@gmail.com>
2019-01-22 11:20:24 -0500

    c

--- src/Projectile.java
+++ src/Projectile.java
@@ -1,12 +1,18 @@
-
+/* [Projectile.java]
+   Object reference for projectile damage dealing object
+   Author: James Liang
+   Date: January 22, 2019
+*/
 public class Projectile extends Item {
 	
+	//Base variables (store tag)
 	private int tag;
 	
 	public int getTag() {
 		return tag;
 	}
 	
+	//Base constructor
 	public Projectile(int x, int y, int height, int width, int radius, double[] speed, boolean affectGravity, int tag) {
 		super(x, y, height, width, radius, speed, affectGravity);
 		this.tag = tag;
