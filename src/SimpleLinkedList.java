17dbbcca54038045225777dbec506de68bab056f
bl21812 <39709270+bl21812@users.noreply.github.com>
2019-01-22 11:25:02 -0500

    c

--- src/SimpleLinkedList.java
+++ src/SimpleLinkedList.java
@@ -1,25 +1,47 @@
+/**
+ * SimpleLinkedList<T>
+ * Simple Linked List object that holds nodes with generic objects
+ * @author Brian Li, James Liang, Michael Oren, Brian Zhang
+ * January 22, 2019
+ */
+
 public class SimpleLinkedList<T> {
   
-  private Node<T> head = null;
+  private Node<T> head = null; //Head node in list
   
+  /**
+   * SimpleLinkedList
+   * Constructor that makes an empty list
+   */
   SimpleLinkedList() {
-  }
+  } //End of constructor
   
+  /**
+   * add
+   * Adds an item to the list
+   * @param item, a generic object T representing item added
+   */
   public void add(T item) {
     
     Node<T> tempNode = head;
     
     if (head == null) {
-      head = new Node<T>(item, null);
+      head = new Node<T>(item, null); //Set in head if list is empty
     } else {
       while (tempNode.getNext() != null) {
         tempNode = tempNode.getNext();
       }
-      tempNode.addNext(item, null);
+      tempNode.addNext(item, null); //Otherwise set to new node at the end
     }
     
-  }
+  } //End of add
   
+  /**
+   * add
+   * Adds an item at the specific index in the list
+   * @param item, a generic object T representing item added
+   * @param index, an int representing index wanted in list
+   */
   public void add(T item, int index) {
     
     int counter = 0;
@@ -43,8 +65,14 @@ public class SimpleLinkedList<T> {
       }
     }
     
-  }
+  } //End of add
   
+  /**
+   * get
+   * Returns item held at specified index
+   * @param index, int representing index of item wanted
+   * @return T item, Generic item representing item returned
+   */
   public T get(int index) {
     
     int counter = 0;
@@ -65,8 +93,14 @@ public class SimpleLinkedList<T> {
       return null;
     }
     
-  }
+  } //End of get
   
+  /**
+   * indexOf
+   * Returns index of a certain item in list
+   * @param item, generic item T representing item searched for
+   * @return int counter, index of the item in list
+   */
   public int indexOf(T item) {
     
     int counter = 0;
@@ -89,8 +123,14 @@ public class SimpleLinkedList<T> {
     }
     
     return -1;
-  }
+  } //End of indexOf
   
+  /**
+   * remove
+   * Removes a specified item from the list
+   * @param item, Generic item T to be removed
+   * @return boolean, true if removed, false if item not found in list
+   */
   public boolean remove(T item) {
     
     Node<T> tempNode = head;
@@ -117,8 +157,14 @@ public class SimpleLinkedList<T> {
     }
     
     return false;
-  }
+  } //End of remove
   
+  /**
+   * remove
+   * Removes an item at a specified index
+   * @param index, int representing index of item to be removed
+   * @return T item, Generic object representing item removed from list
+   */
   public T remove(int index) {
     
     int counter = 0;
@@ -150,8 +196,13 @@ public class SimpleLinkedList<T> {
     }
     
     return null;
-  }
+  } //End of remove
   
+  /**
+   * size
+   * Returns number of items in list
+   * @return int counter, representing size of list
+   */
   public int size() {
     
     int counter = 0;
@@ -167,10 +218,14 @@ public class SimpleLinkedList<T> {
     
     return counter;
     
-  }
+  } //End of size
   
+  /**
+   * clear
+   * Clears the list
+   */
   public void clear() {
     head = null;
-  }
+  } //End of clear
   
-}
+} //End of class
